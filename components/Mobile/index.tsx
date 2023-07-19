cd3943d6f2a525339ff80a94c7f41f42ed49b606
Lorimer Jenkins <90644448+LorimerJenkins@users.noreply.github.com>
2023-07-19 18:23:10 +0100

    feat: placeholder UI

---
+++ components/Mobile/index.tsx
@@ -0,0 +1,25 @@
+import * as Styled from './styles';
+
+const Mobile = () => {
+  return (
+    <Styled.MainWrapper>
+      <Styled.heroContainer>
+        <Styled.oMobile>
+          <h1>Othent Mobile</h1>
+          <p>Othent mobile is currently exclusively supported on the app store and chrome store</p>
+
+          <div className='imgContainer'>
+            <a href="https://example.com" target="_blank" rel="noopener noreferrer">
+              <img src='/downloadOnAppStore.png' />
+            </a>
+            <a href="https://example.com" target="_blank" rel="noopener noreferrer">
+              <img src='/chromeWebStore.png' />
+            </a>
+          </div>
+        </Styled.oMobile>
+      </Styled.heroContainer>
+    </Styled.MainWrapper>
+  );
+};
+
+export default Mobile;
---
+++ components/Mobile/styles.ts
@@ -0,0 +1,58 @@
+import styled from 'styled-components';
+import Button from '../Button'
+import { FeatureHeaderText, FeaturesContainer } from '../common';
+
+
+
+export const MainWrapper = styled(FeaturesContainer)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 10vh;
+  flex-direction: column;
+  background-size: 40% 85%, 110% 80%;
+  background-repeat: no-repeat, no-repeat;
+  background-position: -15% 160%, -380% 80%;
+  background-image: url('/bkg-squares.svg'), url('bkg-circles.svg');
+  margin-top: 0;
+`;
+export const heroContainer = styled.div`
+  height: 90vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+`;
+
+
+
+export const oMobile = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 350px;
+  height: fit-content;
+  background-color: white;
+  padding: 50px;
+  border-radius: 5px;
+  gap: 50px;
+  border: 1px solid black;
+  p {
+    text-align: center;
+  }
+  .imgContainer {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    gap: 10px;
+    img {
+      height: 50px;
+    }
+  }
+`;
+
+
+
